fbc1c564940de2cbe857a1a95131432939e57eff
Quint Burkley <45984165+qwow5@users.noreply.github.com>
2020-05-16 23:03:45 +0000

    Updated build script

--- example_build.sh
+++ example_build.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+# BetterLineage Build Script
+#
+# Customize to your needs.
+# Make sure to install Docker before running this.
+#
+# This script is powered by docker-lineage-cicd by the LineageOS for microG team. Special thanks to them!
+# More information here: https://github.com/lineageos4microg/docker-lineage-cicd
+
 docker run \
     -e "BRANCH_NAME=lineage-16.0" \
+    -e "RELEASE_TYPE=BetterLineage-UNOFFICIAL" \
     -e "DEVICE_LIST=sailfish" \
     -e "ANDROID_JACK_VM_ARGS='-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8G'" \
     -e "CCACHE_SIZE=100G" \
     -e "SIGN_BUILDS=true" \
-    -e "USER_NAME='cid'" \
-    -e "USER_MAIL='cid@lineageos.org'" \
-    -e "KEYS_SUBJECT='/C=US/ST=Washington/L=Seattle/O=LineageOS/OU=BetterLineage/CN=Cid/emailAddress=cid@lineageos.org'" \
+    -e "USER_NAME='android'" \
+    -e "USER_MAIL='android@android.com'" \
     -e "WITH_SU=true" \
     -e "SIGNATURE_SPOOFING=restricted" \
-    -e "CUSTOM_PACKAGES=AdAway Arnova8G2GCamPixel2Mod AuroraServices AuroraStore com.google.android.maps Etar DAVx5 DuckDuckGoPrivacyBrowser FDroid FDroidPrivilegedExtension FakeStore GmsCore GsfProxy MozillaNlpBackend NominatimNlpBackend OpenWeatherProvider PrivacyFriendlyNetMonitor QKSMS SimpleFileManagerPro SimpleNotes " \
+    -e "CUSTOM_PACKAGES=AccountManager additional_repos AuroraServices AuroraStore com.google.android.maps eDrive Etar FDroid FDroidPrivilegedExtension FakeStore FliteTTSEngine GmsCore GsfProxy LibreOfficeViewer MaterialFiles MozillaNlpBackend MuPDFViewer NominatimNlpBackend OmniNotesFOSS OpenCamera OpenTasks OpenWeatherProvider PrivacyBrowser PrivacyFriendlyNetMonitor QKSMS SecScanQR UnlockClock " \
     -v "/home/cid/lineage/build/lineage:/srv/src" \
     -v "/home/cid/lineage/build/zips:/srv/zips" \
     -v "/home/cid/lineage/build/logs:/srv/logs" \
